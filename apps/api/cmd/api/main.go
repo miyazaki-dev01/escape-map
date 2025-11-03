6752738be3c0029b3f7dcf7aa7e6fa756efc2b4a
miyazaki <miyazaki.dev01@gmail.com>
2025-11-03 21:47:10 +0900

    add: APIの疎通テスト処理を実装

--- apps/api/cmd/api/main.go
+++ apps/api/cmd/api/main.go
@@ -1,13 +1,38 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+  "os"
+  "strings"
+  "time"
+
+  "github.com/gin-contrib/cors"
+  "github.com/gin-gonic/gin"
+)
 
 func main() {
-  router := gin.Default()
-  router.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "Hello, World!",
-    })
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  allowOriginEnv := os.Getenv("ALLOW_ORIGIN")
+  if allowOriginEnv == "" {
+    allowOriginEnv = "http://localhost:3000"
+  }
+  allowOrigins := strings.Split(allowOriginEnv, ",")
+
+  r := gin.Default()
+  r.Use(cors.New(cors.Config{
+    AllowOrigins:     allowOrigins,
+    AllowMethods:     []string{"GET"},
+    AllowHeaders:     []string{"Content-Type"},
+    AllowCredentials: true,
+    MaxAge:           12 * time.Hour,
+  }))
+
+  r.GET("/test", func(c *gin.Context) {
+    c.JSON(200, gin.H{"message": "Hello, World!"})
   })
-  router.Run() // デフォルトで0.0.0.0:8080で待機します
+
+  r.Run(":" + port)
 }
